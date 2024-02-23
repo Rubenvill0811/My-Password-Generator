25c0bdeb8bea96309898770797dcc4388a78858d
Rubenvill0811 <rubenvill0811@gmail.com>
2024-02-22 22:01:32 -0800

    added function generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,40 @@
 // Assignment code here
-const upperCaseChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-const lowerCaseChar = ["abcdefghijklmnopqrstuvwxyz"];
+// 
+// function createRandomUc() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+
+// function createRandomLc() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// function createRandomInt() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
+
+var lowerCase = ['a','b','c'];
+var upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+
+function generatePassword() {
+  var lowerCasePrompt = confirm("Do you want to add lowercase letters?")
+  var upperCasePrompt = confirm("Do you want to add uppercase letters?")
+
+if (lowerCasePrompt === true) {
+  // 
+}
+
+  // var numberPrompt = confirm("Would")
+}
+
+function getRandomNumber(min, max) {
+  let step1 = max - min  + 1;
+  let step2 = Math.random() * step1;
+  let result = Math.floor(step2) + min;
+
+  return result;
+}
+
+console.log(lowerCase);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
