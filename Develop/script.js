c6b49db7f8f5cedecd5eaa05a55c8f6d5f37e31d
Rubenvill0811 <rubenvill0811@gmail.com>
2024-02-25 19:41:04 -0800

    added while loop and logic for the prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,25 @@
 // Game Plan
 // when the user enters the site, they will be prompted for the criteria of their password. They will be asked if they want lowercase letters, uppercase letters, numbers, and symnbols. If yes, then it will be included in the generated password. If no, then it will be excluded. Once the criteria has been set a function will select random items out of their respective arrays and string them together randomly to to create a secure password.
 
-// var lowerCasePrompt = confirm("Do you want to add lowercase letters?");
-// var upperCasePrompt = confirm("Do you want to add uppercase letters?");
-// var numberPrompt = confirm("Do you want to add numbers?");
-// var symbolPrompt = confirm("Do you want add symbols?");
 
-const uppercaseEl = document.getElementById('')
+const genLength = document.getElementById('chosen-length');
+genLength.addEventListener("change", function() {
+  
+  console.log(genLength.value);
+})
 
 
 const randomFunc = {
-  number: grabRandomNumber,
-  upper: grabRandomUpperCase,
-  lower: grabRandomLowerCase,
-  symbol: grabRandomSymbol
+  number: grabRandomNumber(),
+  upper: grabRandomUpperCase(),
+  lower: grabRandomLowerCase(),
+  symbol: grabRandomSymbol()
 };
+
+function passCharLength() {
+  const charLength = genLength;
+  console.log(charLength);
+}
 
 function grabRandomNumber() {
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];  
@@ -26,6 +31,7 @@ function grabRandomUpperCase() {
   return uppercase[Math.floor(Math.random() * uppercase.length)]; 
 }
 
+
 function grabRandomLowerCase() {
   const lowerCasecase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  
   return lowerCasecase[Math.floor(Math.random() * lowerCasecase.length)]; 
@@ -36,21 +42,33 @@ function grabRandomSymbol() {
   return symbol[Math.floor(Math.random() * symbol.length)]; 
 }
 
-// My code ^
-
-console.log(grabRandomSymbol());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var lowerCasePrompt = false;
+  var upperCasePrompt = false;
+  var numberPrompt = false;
+  var symbolPrompt = false;
+  while(!(lowerCasePrompt || upperCasePrompt || numberPrompt || symbolPrompt)) {
+    lowerCasePrompt = confirm("Do you want to add lowercase letters?");
+    upperCasePrompt = confirm("Do you want to add uppercase letters?");
+    numberPrompt = confirm("Do you want to add numbers?");
+    symbolPrompt = confirm("Do you want add symbols?");
+    if (!(lowerCasePrompt || upperCasePrompt || numberPrompt || symbolPrompt)){
+      alert("You must select one option to generate a password.");
+    }
+  }
   passwordText.value = password;
-
+  
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
