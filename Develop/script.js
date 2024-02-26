9675f4cdf5ced074d9e9023018d18a1c70f27443
Rubenvill0811 <rubenvill0811@gmail.com>
2024-02-25 18:07:02 -0800

    added the const randomFucn and added an input in html for the length selector,

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,23 @@
 // Game Plan
 // when the user enters the site, they will be prompted for the criteria of their password. They will be asked if they want lowercase letters, uppercase letters, numbers, and symnbols. If yes, then it will be included in the generated password. If no, then it will be excluded. Once the criteria has been set a function will select random items out of their respective arrays and string them together randomly to to create a secure password.
 
-// var lowerCase = ['a','b','c',"d","e","f","g","h","i","j","k",];
-// var charList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", "-", "_", "=", "+" ];
-
-// Lowercase letters are 0-25, Uppercase letters are 26-51, Numbers are  51-61, symbols are 62-79. charList array comes to a total of 80.
-
-console.log(grabRandomLowerCase());
-
-
 // var lowerCasePrompt = confirm("Do you want to add lowercase letters?");
 // var upperCasePrompt = confirm("Do you want to add uppercase letters?");
 // var numberPrompt = confirm("Do you want to add numbers?");
 // var symbolPrompt = confirm("Do you want add symbols?");
 
-// let lowerAnswer = lowerCasePrompt;
-// let upperAnswer = upperCasePrompt;
-// let numberAnswer = numberPrompt;
-// let symbolAnswer = symbolPrompt;
+const uppercaseEl = document.getElementById('')
+
+
+const randomFunc = {
+  number: grabRandomNumber,
+  upper: grabRandomUpperCase,
+  lower: grabRandomLowerCase,
+  symbol: grabRandomSymbol
+};
 
 function grabRandomNumber() {
-  const numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];  
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];  
   return numbers[Math.floor(Math.random() * numbers.length)]; 
 }
 
@@ -35,18 +32,13 @@ function grabRandomLowerCase() {
 }
 
 function grabRandomSymbol() {
-  const symbol = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", ";", ":", "<", ">", "?", "~"];  
-  return symbol[Math.floor(Math.random() * lowerCasecase.length)]; 
+  const symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", ";", ":", "<", ">", "?", "~"];  
+  return symbol[Math.floor(Math.random() * symbol.length)]; 
 }
-// function getRandomNumber(min, max) {
-//   let step1 = max - min  + 1;
-//   let step2 = Math.random() * step1;
-//   let result = Math.floor(step2) + min;
 
-//   return result;
-// }
+// My code ^
 
-
+console.log(grabRandomSymbol());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
